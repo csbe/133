1eeba0c563949b8fc79394f88c06abab0dacf527
root <root@note1.web4b.local>
2015-12-23 11:53:25 +0100

    several warenkorb changes

--- ecommerce/view/productliste.php
+++ ecommerce/view/productliste.php
@@ -11,9 +11,36 @@
 			</figure>
 			<p><?php echo $product->getDescription() ?></p>	
 			<span class="preis"><?php echo $product->getPreis() ?> CHF</span>	
-			<span class="addtowarenkorb"><a href="?controller=Warenkorb&action=add&id=<?php echo $product->getId() ?>" onclick="$.ajax(this.href).done(function(responseText){$('#warenkorb').html(responseText);});;return false;"><img src="images/warenkorb.gif" alt="Add to Warenkorb"></a></span>	
+			<span class="addtowarenkorb"><a href="?controller=Warenkorb&action=add&id=<?php echo $product->getId() ?>" id="<?php echo $product->getId() ?>" onclick="return false;"><img src="images/warenkorb.gif" alt="Add to Warenkorb"></a></span>	
 		</article>			
 		<?php
 	}
 ?>
 </section>
+
+<script type="text/javascript">
+	
+	$("article .addtowarenkorb a").click(function () {
+		$.ajax(this.href).done(function(responseText){$('#warenkorb').html(responseText);});
+		
+		$xwarenkorb = $("#warenkorb").offset().left; //top
+		$ywarenkorb = $("#warenkorb").offset().top;
+		if ($("#artikel"+$(this).attr('id')).length>0) {
+			$xwarenkorb = $("#artikel"+$(this).attr('id')).offset().left; //top
+			$ywarenkorb = $("#artikel"+$(this).attr('id')).offset().top;			
+		}
+		$xarticle = $(this).offset().left;
+		$yarticle = $(this).offset().top;
+		$x = $xwarenkorb - $xarticle;
+		$y = $ywarenkorb - $yarticle + 20;
+		$clone = $(this).parent().clone();
+		$(this).parent().parent().append($clone);
+		$clone.animate({'left':$x, 'top':$y, 'opacity':'0.5'},600).animate({'opacity':'0'},300, 'linear', function () {
+			$clone.remove();
+		});
+		//$(this).hide();
+		//$(this).animate({"display": "none" },150);	
+		
+		return false;
+	});
+</script>
--- ecommerce/view/warenkorb.php
+++ ecommerce/view/warenkorb.php
@@ -1,10 +1,13 @@
 <h4>Warenkorb</h4>
 
 <?php
-
+	$count = 0;
+	$preis = 0;
 	foreach($this->warenkorb->get() as $key => $value){
 		echo $key . ' - ' . $value['anzahl'] . '*' . $value['article']->getPreis() . '=' . $value['article']->getPreis()*$value['anzahl'] . 'CHF';
-		echo '<span class="addtowarenkorb"><a href="?controller=Warenkorb&action=remove&id='.$value['article']->getId() .'" onclick="$.ajax(this.href).done(function(responseText){$(\'#warenkorb\').html(responseText);});;return false;"><img src="images/loeschen.jpg" alt="Remove from Warenkorb"></a></span><br>';	
+		echo '<span class="addtowarenkorb"><a href="?controller=Warenkorb&action=remove&id='.$value['article']->getId() .'" onclick="$.ajax(this.href).done(function(responseText){$(\'#warenkorb\').html(responseText);}); return false;" id="artikel'.$value['article']->getId().'"><img src="images/loeschen.jpg" alt="Remove from Warenkorb"></a></span><br>';	
+		$count += $value['anzahl'];
+		$preis += $value['anzahl']*$value['article']->getPreis();
 	}
-
+	printf("<br>%d Artikel f&uuml;r %sCHF", $count, number_format($preis,2,'.','\''));
 ?>
